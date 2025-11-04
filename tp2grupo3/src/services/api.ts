de81c752e90cdd3a22f1e91baeed78500982c674
Pablo Cortese <cortesepablonahuel@gmail.com>
2025-11-03 22:23:24 -0300

    agregado de servicios de consultas al api

---
+++ tp2grupo3/src/services/api.ts
@@ -0,0 +1,88 @@
+import axios from 'axios';
+import type {
+  User,
+  Post,
+  Comment,
+  Tag,
+  PostImage,
+  CreatePostData,
+  CreateCommentData,
+  CreatePostImageData
+} from '../types';
+
+const API_BASE_URL = 'http://localhost:3001';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Usuarios
+export const getUsers = async (): Promise<User[]> => {
+  const response = await api.get<User[]>('/users');
+  return response.data;
+};
+
+export const getUserById = async (id: number): Promise<User> => {
+  const response = await api.get<User>(`/users/${id}`);
+  return response.data;
+};
+
+export const createUser = async (nickName: string, email: string): Promise<User> => {
+  const response = await api.post<User>('/users', { nickName, email });
+  return response.data;
+};
+
+// Publicaciones
+export const getPosts = async (): Promise<Post[]> => {
+  const response = await api.get<Post[]>('/posts');
+  return response.data;
+};
+
+export const getPostsByUserId = async (userId: number): Promise<Post[]> => {
+  const response = await api.get<Post[]>(`/posts?userId=${userId}`);
+  return response.data;
+};
+
+export const getPostById = async (id: number): Promise<Post> => {
+  const response = await api.get<Post>(`/posts/${id}`);
+  return response.data;
+};
+
+export const createPost = async (data: CreatePostData): Promise<Post> => {
+  const response = await api.post<Post>('/posts', data);
+  return response.data;
+};
+
+// Comentarios
+export const getCommentsByPostId = async (postId: number): Promise<Comment[]> => {
+  const response = await api.get<Comment[]>(`/comments/post/${postId}`);
+  return response.data;
+};
+
+export const createComment = async (data: CreateCommentData): Promise<Comment> => {
+  const response = await api.post<Comment>('/comments', data);
+  return response.data;
+};
+
+// Etiquetas
+export const getTags = async (): Promise<Tag[]> => {
+  const response = await api.get<Tag[]>('/tags');
+  return response.data;
+};
+
+// Imágenes
+export const getImagesByPostId = async (postId: number): Promise<PostImage[]> => {
+  const response = await api.get<PostImage[]>(`/postimages/post/${postId}`);
+  return response.data;
+};
+
+export const createPostImage = async (data: CreatePostImageData): Promise<PostImage> => {
+  const response = await api.post<PostImage>('/postimages', data);
+  return response.data;
+};
+
+export default api;
+
